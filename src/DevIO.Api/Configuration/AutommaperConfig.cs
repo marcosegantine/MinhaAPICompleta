353b5b2814d184bc1145b0ee47e5581535b2b602
Marcos Segantine <marcosegantine@hotmail.com>
2023-07-19 20:09:53 +0100

    Add project files.

---
+++ src/DevIO.Api/Configuration/AutommaperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DevIO.Api.Models;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Configuration
+{
+    public class AutommaperConfig : Profile
+    {
+        public AutommaperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificaçoes de erro
+
+        //validação de modelstate
+
+        //validação da operação de negocios
+
+    }
+}
